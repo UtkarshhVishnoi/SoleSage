3289c7069024d43b86dcfe064ba64fa7092882e3
Utkarsh Vishnoi <118669807+UtkarshhVishnoi@users.noreply.github.com>
2025-06-03 17:14:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,83 +1,81 @@
+let menu_btn = document.getElementById("menu-btn");
+let drop_menu = document.getElementById("drop_nav");
 
+menu_btn.addEventListener("click", () => {
+  drop_menu.classList.toggle("hidden");
+});
 
-let menu_btn = document.getElementById('menu-btn');
-let drop_menu = document.getElementById('drop_nav');
-
-menu_btn.addEventListener('click', () => {
-    drop_menu.classList.toggle("hidden")
-
-})
-
-
-
-let productName = '';
-let productPrice = '';
-let image = '';
+let productName = "";
+let productPrice = "";
+let image = "";
 let cartnum = 0;
 let cartprice = 0;
 
-const productsContainer = document.getElementById('product-container');
-productsContainer.addEventListener('click', function (event) {
-    const clickedElement = event.target;
+const productsContainer = document.getElementById("product-container");
+productsContainer.addEventListener("click", function (event) {
+  const clickedElement = event.target;
 
-
-    if (clickedElement.classList.contains('add-t-cart')) {
-
-        const productCard = clickedElement.closest('.pcard');
-        if (productCard) {
-            productName = productCard.querySelector('.product-title').textContent;
-            productPrice = Number(productCard.querySelector('.product-price').textContent);
-            image = productCard.querySelector('.product-image').src;
-            cartnum += 1;
-            cartprice += productPrice;
-            targetElement = document.getElementById('crt');
-            document.getElementById('cartp').innerText = cartprice;
-            const newHtmlContent = `
+  if (clickedElement.classList.contains("add-t-cart")) {
+    const productCard = clickedElement.closest(".pcard");
+    if (productCard) {
+      productName = productCard.querySelector(".product-title").textContent;
+      productPrice = Number(
+        productCard.querySelector(".product-price").textContent
+      );
+      image = productCard.querySelector(".product-image").src;
+      cartnum += 1;
+      cartprice += productPrice;
+      targetElement = document.getElementById("crt");
+      document.getElementById("cartp").innerText = cartprice;
+      const newHtmlContent = `
                        <div class='flex items-center gap-2 rounded-md p-2'>
                             <img src=${image} class='h-28 w-28'/>
                             <div class='flex gap-3 items-center'>
                                <h1 class='text-md font-semibold'>${productName}</h1>
-                               <p>${'$' + productPrice}</p>
+                               <p>${"₹" + productPrice}</p>
                             </div>
                         </div>`;
 
+      targetElement.innerHTML += newHtmlContent;
 
-            targetElement.innerHTML += newHtmlContent;
-            
-            console.log(`Adding to cart: Name: ${productName}, Price: ${productPrice} , cartprice: ${cartprice} , cart: ${cartnum} , src: ${image} `);
+      console.log(
+        `Adding to cart: Name: ${productName}, Price: ${productPrice} , cartprice: ${cartprice} , cart: ${cartnum} , src: ${image} `
+      );
 
-            document.getElementById('cart-num').innerText = cartnum;
-
-        }
+      document.getElementById("cart-num").innerText = cartnum;
     }
-
+  }
+  Toastify({
+    text: "Item added to cart",
+    duration: 3000,
+    newWindow: true,
+    close: true,
+    gravity: "bottom", // `top` or `bottom`
+    position: "right", // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {
+      background: "#603F26",
+      borderRadius: "20px",
+      color: "#f7f3e7",
+    },
+    onClick: function () {}, // Callback after click
+  }).showToast();
 });
 
-
-
-    const cartContainer = document.getElementById('crt'); 
-    if (cartContainer) {
-        
-        cartContainer.addEventListener('click', function(event) {
-            const clickedElement = event.target; 
-            if (clickedElement.id === 'crt-btn') {
-                document.getElementById('crt').classList.toggle("hidden")
-            }
-            
-        });
-    } 
-
-document.getElementById('cart_icon_desktop').addEventListener(
-    'click', () => {
-        document.getElementById('crt').classList.toggle("hidden")
-
+const cartContainer = document.getElementById("crt");
+if (cartContainer) {
+  cartContainer.addEventListener("click", function (event) {
+    const clickedElement = event.target;
+    if (clickedElement.id === "crt-btn") {
+      document.getElementById("crt").classList.toggle("hidden");
     }
-)
+  });
+}
 
-document.getElementById('cart_icon_sm').addEventListener(
-    'click', () => {
-        document.getElementById('crt').classList.toggle("hidden")
+document.getElementById("cart_icon_desktop").addEventListener("click", () => {
+  document.getElementById("crt").classList.toggle("hidden");
+});
 
-    }
-)
-
+document.getElementById("cart_icon_sm").addEventListener("click", () => {
+  document.getElementById("crt").classList.toggle("hidden");
+});
